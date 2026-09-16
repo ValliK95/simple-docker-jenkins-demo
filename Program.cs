3d7335ff092ba6d18522e397cf65af976142ce4e
Valli Kumar <95valli@gmail.com>
2026-09-16 15:32:26 +0400

    Add hello endpoint

--- Program.cs
+++ Program.cs
@@ -33,6 +33,8 @@ app.MapGet("/weatherforecast", () =>
 })
 .WithName("GetWeatherForecast");
 
+app.MapGet("/hello", () => "Hello from the Jenkins pipeline!");
+
 app.Run();
 
 record WeatherForecast(DateOnly Date, int TemperatureC, string? Summary)
